75f40cc22fcfb1994074fcb31cc326a4f185e9b8
lhy_oe1 <anan210607@163.com>
2025-12-23 17:33:00 +0800

    week1

---
+++ UP progress/week1/1.higher-order functions.py
@@ -0,0 +1,35 @@
+#高阶函数
+from functools import reduce
+# map()  reduce()
+
+#
+def f(x):
+    return x*x
+
+r=map(f,[x for x in range(9)])
+print(r)            # 此时的r  是一个 map 迭代器
+print(list(r))      #使用list（） 将整个序列计算返回
+
+l=list(map(str,[1,2,3,4]))
+print(l)
+
+# reduce
+# 函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
+import math
+from functools import reduce
+def add(x, y):
+    return x + y
+
+reduce(add, [1, 3, 5, 7, 9])
+
+from functools import reduce
+
+DIGITS = {'a': 0, 'b': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def char2num(s):
+    return DIGITS[s]
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+print(str2int('a'))
---
+++ UP progress/week1/test1.py
@@ -0,0 +1,37 @@
+#1. 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+def normalize(name):
+    return name.capitalize()
+
+# 测试:
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+#2. Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+from functools import reduce
+
+def s(x,y):
+    return x*y
+def prod(L):
+    return reduce(s,L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+from functools import reduce
+
+def str2float(s):
+    pass
+
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')
