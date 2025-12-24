7cf828a0e335b05d1d90da43a8729401c28c43df
lhy_oe1 <anan210607@163.com>
2025-12-24 17:32:52 +0800

    week1

--- UP progress/week1/1.1 高级函数.py
+++ UP progress/week1/1.1 高级函数.py
@@ -33,3 +33,14 @@ def str2int(s):
     return reduce(lambda x, y: x * 10 + y, map(char2num, s))
 
 print(str2int('a'))
+
+## filter(fn,l)
+# 和map()类似，filter()也接收一个函数和一个序列。和map()不同的是，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素
+def is_odd(n):
+    return n % 2 == 1
+
+list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15]))
+[1, 5, 9, 15]
+
+
+# sorted(l,key=,reverse)
